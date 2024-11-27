007d7f1c247536744c86e188511c49c71997363a
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-11-27 20:26:34 +0300

    feat(form): Реализована валидация поля username

--- 2-hw-form-validation/src/components/form.svelte
+++ 2-hw-form-validation/src/components/form.svelte
@@ -12,43 +12,58 @@
     confirmPassword: FormField;
   }
 
-  let username = $state("");
-  let password = $state("");
-  let confirmPassword = $state("");
+  let username = $state({
+    value: "",
+    errors: [],
+    isValidating: false,
+    touched: false,
+  });
 
-  let formData: FormState = $state({
-    username: {
-      value: username,
-      errors: [],
-      isValidating: false,
-      touched: false,
-    },
-    password: {
-      value: password,
-      errors: [],
-      isValidating: false,
-      touched: false,
-    },
-    confirmPassword: {
-      value: confirmPassword,
-      errors: [],
-      isValidating: false,
-      touched: false,
-    },
-  })
+  let password = $state({
+    value: "",
+    errors: [],
+    isValidating: false,
+    touched: false,
+  });
 
+  let confirmPassword = $state({
+    value: "",
+    errors: [],
+    isValidating: false,
+    touched: false,
+  });
+
+  let formData: FormState = $derived({ username, password, confirmPassword })
+
+  // Валидация поля username
   const handleUsernameInputChange = (e: Event) => {
     const target = e.target as HTMLInputElement;
-    if (target) {
-      const onlyLettersMoreThanTwoRegExp = /^[a-zA-Z]{2,}$/;
-      formData.username.isValidating = onlyLettersMoreThanTwoRegExp.test(target.value);
 
+    if (target) {
+      // Проверка на длину строки
+      if (target.value.length <= 1 ) {
+        if (!formData.username.errors.includes("Username must be at least 2 characters")){
+          formData.username.errors.push("Username must be at least 2 characters");
+        }
+      } else {
+        formData.username.errors = formData.username.errors.filter((error) => error !== "Username must be at least 2 characters");
+      }
+
+      // Проверка на отсутствие любых символов кроме букв
+      const onlyLettersRegExp = /^[a-zA-Z]+$/;
+
+      if (!onlyLettersRegExp.test(target.value)) {
+        if(!formData.username.errors.includes("Username must have letter only")) {
+          formData.username.errors.push("Username must have letter only");
+        }
+      } else {
+        formData.username.errors = formData.username.errors.filter((error) => error !== "Username must have letter only");
+      }
+
+      // Установка/снятие флага валидации
+      formData.username.isValidating = formData.username.errors.length === 0;
     }
   };
-
-  // const handleUsernameInputFocus = () => {
-  //   console.log("username field in focus");
-  // }
 
   const handleFormSubmit = (e: SubmitEvent) => {
     e.preventDefault();
@@ -61,30 +76,40 @@
 </script>
 
 <form onsubmit={e => handleFormSubmit(e)}>
-  <p>
-    <label for="username" class:not-valid={formData.username.touched && !formData.username.isValidating}>Username:</label>
+  <div>
+    <label
+      for="username"
+      class:not-valid={formData.username.touched && !formData.username.isValidating}
+    >
+      Username:
+    </label>
     <input
       type="text"
       id="username"
-      bind:value={username}
+      bind:value={username.value}
       oninput={e => handleUsernameInputChange(e)}
       onfocus={() => formData.username.touched = true}
     />
-  </p>
-  <p>
+    {#if (formData.username.errors.length > 0)}
+      <span class="error-message">{formData.username.errors.join(", ")}</span>
+    {/if}
+  </div>
+
+  <div>
     <!-- todo в конце решения задания можно заменить типы с type="text" на type="password" -->
     <label for="password">Password:</label>
-    <input type="text" id="password" bind:value={password}/>
-  </p>
-  <p>
+    <input type="text" id="password" bind:value={password.value}/>
+  </div>
+  <div>
     <label for="confirmPassword">Confirm password:</label>
-    <input type="text" id="confirmPassword" bind:value={confirmPassword}/>
-  </p>
+    <input type="text" id="confirmPassword" bind:value={confirmPassword.value}/>
+  </div>
   <input type="submit" value="Submit">
 </form>
 
 <style>
   form {
+    min-width: 400px;
     border: 5px solid gray;
     border-radius: 40px;
     display: flex;
@@ -94,8 +119,19 @@
     padding: 50px;
   }
 
+  div {
+    margin-bottom: 20px;
+    text-align: right;
+  }
+
+  .error-message {
+    text-align: right;
+    display: block;
+    font-size: 11px;
+    color: red;
+  }
+
   .not-valid {
     color: red;
   }
 </style>
-
